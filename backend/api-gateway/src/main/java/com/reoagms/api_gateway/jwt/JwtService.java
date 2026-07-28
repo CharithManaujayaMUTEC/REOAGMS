a57639e266b55663c4534b8582033a1d633436e8
CharithManaujayaMUTEC <charithmanujaya1@gmail.com>
2026-07-28 15:26:40 +0530

    api gateway coding new 2

--- backend/api-gateway/src/main/java/com/reoagms/api_gateway/jwt/JwtService.java
+++ backend/api-gateway/src/main/java/com/reoagms/api_gateway/jwt/JwtService.java
@@ -2,8 +2,8 @@ package com.reoagms.api_gateway.jwt;
 
 public interface JwtService {
 
-    String extractUsername(String token);
-
     boolean isTokenValid(String token);
+
+    String extractUsername(String token);
 
 }
--- backend/api-gateway/src/main/java/com/reoagms/api_gateway/jwt/JwtServiceImpl.java
+++ backend/api-gateway/src/main/java/com/reoagms/api_gateway/jwt/JwtServiceImpl.java
@@ -1,1 +1,66 @@
 package com.reoagms.api_gateway.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+@Service
+@RequiredArgsConstructor
+public class JwtServiceImpl implements JwtService {
+
+    private final JwtProperties jwtProperties;
+
+    private SecretKey getSigningKey() {
+
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtProperties.getSecretKey())
+        );
+
+    }
+
+    @Override
+    public boolean isTokenValid(String token) {
+
+        try {
+
+            extractClaims(token);
+
+            return true;
+
+        }
+
+        catch (Exception ex) {
+
+            return false;
+
+        }
+
+    }
+
+    @Override
+    public String extractUsername(String token) {
+
+        return extractClaims(token).getSubject();
+
+    }
+
+    private Claims extractClaims(String token) {
+
+        return Jwts.parser()
+
+                .verifyWith(getSigningKey())
+
+                .build()
+
+                .parseSignedClaims(token)
+
+                .getPayload();
+
+    }
+
+}
